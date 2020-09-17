93c3d0632326cffedd04d1fceb966cb80f3da67b
FedoraMark <fedora.mark@icloud.com>
2020-09-17 05:44:04 -0400

    start functionality

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Magicount from './magiCount.js';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Magicount default={20} />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/magiCount.js
@@ -0,0 +1,153 @@
+import React, { Component } from "react";
+import classnames from "classnames";
+import PropTypes from "prop-types";
+import Animated from "react-css-animated";
+import { useSwipeable, Swipeable } from "react-swipeable";
+
+import { AiFillPlusCircle, AiFillMinusCircle } from "react-icons/ai";
+
+import "animate.css";
+import style from "./magiCount.module.scss";
+
+const ANIM = "animate__animated animate__";
+
+const defaultScores = [10, 20, 30, 40, 50];
+
+const LEFT = "left";
+const RIGHT = "right";
+
+const DURATION = 400;
+
+class magiCount extends Component {
+  static propTypes = {
+    default: PropTypes.number.isRequired,
+    warnAt: PropTypes.number,
+    dangerAt: PropTypes.number,
+  };
+
+  static defaultProps = {
+    warnAt: 5,
+    dangerAt: 0,
+  };
+
+  state = {
+    score: this.props.default,
+    atDefault: true,
+    defaultIndex: defaultScores.indexOf(this.props.default),
+    swipeDirection: "",
+    isResetting: true,
+    transitionScore: false,
+  };
+
+  componentDidMount() {
+    this.setState({ isResetting: false });
+  }
+
+  // FUNCITONS
+  changeBy = (amt) => {
+    // use NEGATIVES to decrement
+    this.setState({ atDefault: false, score: this.state.score + amt });
+  };
+
+  scoreSwipe = (dir) => {
+    if (!this.state.atDefault) {
+      return;
+    }
+
+    let newIndex = 0;
+    if (dir === LEFT) {
+      newIndex = this.state.defaultIndex + 1;
+    } else if (dir === RIGHT) {
+      newIndex = this.state.defaultIndex - 1;
+    }
+
+    if (newIndex < 0) {
+      newIndex = defaultScores.length - 1;
+    } else if (newIndex >= defaultScores.length) {
+      newIndex = 0;
+    }
+
+    this.setState(
+      {
+        swipeDirection: dir,
+        transitionScore: true,
+      },
+      () => {
+        setTimeout(() => {
+          this.setState({
+            transitionScore: false,
+            defaultIndex: newIndex,
+            score: defaultScores[newIndex],
+          });
+        }, DURATION/2);
+      }
+    );
+  };
+
+  // RENDERERS
+  render() {
+    let exitAnim = this.state.swipeDirection === LEFT ? "fadeOutLeftBig" : "fadeOutRightBig";
+    let enterAnim = this.state.swipeDirection === LEFT ? "bounceInRight" : "bounceInLeft";
+
+    return (
+      <div
+        className={classnames(
+          style.mc_wrapper,
+          this.state.atDefault ? style.default : style.normal,
+          this.state.score <= this.props.warnAt ? style.warning : "",
+          this.state.score <= this.props.dangerAt ? style.danger : ""
+        )}
+      >
+        {/* PLUS BUTTON */}
+        <div
+          className={classnames(style.button, style.plusButton)}
+          onClick={this.changeBy.bind(this, 1)}
+        >
+          <div className={style.icon}>
+            <AiFillPlusCircle />
+          </div>
+        </div>
+
+        {/* SCORE AREA */}
+        <Swipeable
+          className={classnames(style.area, style.scoreArea)}
+          onSwipedLeft={this.scoreSwipe.bind(this, LEFT)}
+          onSwipedRight={this.scoreSwipe.bind(this, RIGHT)}
+          trackMouse
+          trackTouch
+          preventDefaultTouchmoveEvent
+        >
+          <Animated
+            className={classnames(style.score)}
+            animationIn={this.state.isResetting ? "fadeIn" : enterAnim}
+            animationOut={this.state.isResetting ? "fadeOut" : exitAnim}
+            animateOnMount
+            isVisible={!this.state.transitionScore}
+            duration={DURATION}
+            easing={this.state.transitionScore ? "ease-in" : "ease"}
+          >
+            {this.state.score.toString().split("").map((digit, index) => {
+                return (
+                  <span key={index} className={style.digit}>
+                    {digit}
+                  </span>
+                );
+              })}
+          </Animated>
+        </Swipeable>
+
+        {/* MINUS BUTTON */}
+        <div
+          className={classnames(style.button, style.minusButton)}
+          onClick={this.changeBy.bind(this, -1)}
+        >
+          <div className={style.icon}>
+            <AiFillMinusCircle />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default magiCount;
